f3769673539ec788fa3ec015890e2fcac3e11246
Mujeeb1408 <mujeebattar28@gmail.com>
2023-08-23 14:34:13 +0530

    made some Updates

--- Dom Task 3/dom.js
+++ Dom Task 3/dom.js
@@ -29,7 +29,12 @@ mainheader.style.borderLeft = 'solid 3px red';
 // additem[0].style.fontWeight = 'bold';
 
 var item = document.getElementsByClassName('list-group-item');
-item[0].style.color = 'red';
-item[1].innerHTML = "Hello"
-item[2].style.backgroundColor = 'black';
-item[2].style.color = 'white';
+// item[0].style.color = 'red';
+// item[1].innerHTML = "Hello"
+// item[2].style.backgroundColor = 'black';
+item[2].style.backgroundColor = 'green';
+
+for (var i = 0; i < item.length; i++) {
+    item[i].style.fontWeight = 'bold';
+}
+

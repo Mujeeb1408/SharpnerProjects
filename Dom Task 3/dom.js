96592e7abf55f1c9ef7befdd72216dd2f5afc137
Mujeeb1408 <mujeebattar28@gmail.com>
2023-08-23 18:06:47 +0530

    QuerySelectorAll

--- Dom Task 3/dom.js
+++ Dom Task 3/dom.js
@@ -28,19 +28,37 @@ mainheader.style.borderLeft = 'solid 3px red';
 // additem[0].style.color = 'green';
 // additem[0].style.fontWeight = 'bold';
 
-var item = document.getElementsByClassName('list-group-item');
+// var item = document.getElementsByClassName('list-group-item');
 // item[0].style.color = 'red';
 // item[1].innerHTML = "Hello"
 // item[2].style.backgroundColor = 'black';
-item[2].style.backgroundColor = 'green';
+// item[2].style.backgroundColor = 'green';
 
-for (var i = 0; i < item.length; i++) {
-    item[i].style.fontWeight = 'bold';
-}
+// for (var i = 0; i < item.length; i++) {
+//     item[i].style.fontWeight = 'bold';
+// }
 
 // GetElementByTagName
 
-var li = document.getElementsByTagName('li');
-for (var i = 0; i < li.length; i++) {
-    li[i].style.fontWeight = 'bold';
+// var li = document.getElementsByTagName('li');
+// for (var i = 0; i < li.length; i++) {
+//     li[i].style.fontWeight = 'bold';
+// }
+
+//QuerySelector
+// var item2 = document.querySelector('.list-group-item:nth-child(2)');
+// item2.style.backgroundColor = 'green';
+// var item3 = document.querySelector('.list-group-item:nth-child(3)');
+// item3.style.color = 'white';
+
+//QuerySelectorAll
+//changing 2nd li green
+var items1 = document.querySelectorAll('li');
+items1[1].style.color = 'green';
+console.log(items[2]);
+
+// changing odd background green
+var items = document.querySelectorAll('li:nth-child(odd)');
+for (var i = 0; i < items.length; i++) {
+    items[i].style.backgroundColor = 'green';
 }
